a454da13caa475b06e6c961000e487ec76d330ae
Lucas Remedi Markarian Pavão <lucas.r.pavao@hotmail.com>
2026-03-24 19:43:55 -0300

    Estrutura do projeto

---
+++ src/test/java/br/com/tech/challenger/api_restaurante/ApiRestauranteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tech.challenger.api_restaurante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestauranteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
